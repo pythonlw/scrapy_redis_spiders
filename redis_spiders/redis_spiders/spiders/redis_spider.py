248c6e3fd0140150072d01d9c7e7c16f02ef8b16
lw <1540310556@qq.com>
2021-01-13 19:32:05 +0800

    scrapy-redis 重写start_request方法

--- redis_spiders/redis_spiders/spiders/redis_spider.py
+++ redis_spiders/redis_spiders/spiders/redis_spider.py
@@ -4,6 +4,7 @@ import json
 import scrapy
 from scrapy_redis.spiders import RedisSpider
 
+
 """
 lpush 'redis_spider_key' '{"url":"http://www.baidu.com/"}'
 """
@@ -14,17 +15,15 @@ from scrapy.http import Request
 # from scrapy.utils.project import get_project_settings #读取settings属性
 # CONCURRENT_REQUESTS=get_project_settings().get('CONCURRENT_REQUESTS')
 
-
+#重写startrequest函数
 class RedisSpiderSpider(RedisSpider):
     name = 'redis_spider'
     allowed_domains = ['www.baidu.com']
-    start_urls = ['http://www.baidu.com/']
+    start_urls = []
     redis_key = 'redis_spider_key'
 
     def start_requests(self):
-        return self.next_requests()
-
-    def next_requests(self):
+        #从redis队列读取url
         CONCURRENT_REQUESTS=5
         found = 0
         while found < CONCURRENT_REQUESTS:
@@ -39,19 +38,26 @@ class RedisSpiderSpider(RedisSpider):
                 found += 1
                 print('found:',found)
 
+        #只运行一次
+        # obj1 = {'url': 'http://www.baidu.com'}
+        # print('obj1:',obj1)
+        # req = self.make_requests_from_url(json.dumps(obj1))
+        # if req:
+        #     yield req
+
     def make_requests_from_url(self, url):
         url=json.loads(url)
         item0 = {'url': url['url']}
         headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4115.0 Safari/537.36'
         }
         return Request(url=url['url'], headers=headers,
-                       meta={'item0': item0, 'headers': headers},dont_filter=True)
+                meta={'item0': item0, 'headers': headers},dont_filter=True)
 
     def parse(self, response):
         print('response:',response.url)
         print(response.status)
         print(dir(response))
-
+        print('headers:',response.request.headers)
 
 
 
--- redis_spiders/redis_spiders/spiders/redis_url.py
+++ redis_spiders/redis_spiders/spiders/redis_url.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*- 
 import redis
+def write_url():
+    r=redis.Redis()
+    import json
+    url=json.dumps({"url":"http://www.baidu.com/"})
+    for i in range(50):
+        r.lpush('redis_spider_key',url)
 
-r=redis.Redis()
-import json
-url=json.dumps({"url":"http://www.baidu.com/"})
-
-for i in range(50):
-    r.lpush('redis_spider_key',url)
-
-
+# write_url()
 
 
 
